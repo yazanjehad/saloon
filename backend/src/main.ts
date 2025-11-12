d201655b32a50fb21bc64aff324dacf7ed4e9ce1
yazan <kamsehya@gmail.com>
2025-11-12 16:14:31 +0300

    Change_UserModule

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // ConfigService
+  const configService = app.get(ConfigService);
+
+  // ConfigService
+  const port = configService.get<number>('app.port') || 3000;
+
+  await app.listen(port);
+  console.log(` Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
-import { Role } from './entities/role.entity';
+
+
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role])],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
