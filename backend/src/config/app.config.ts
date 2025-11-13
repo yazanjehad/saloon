550d7bd948cfec77ae403a22fc6bf1563a8dd857
Odai <odaiabd48@gmail.com>
2025-11-13 13:06:04 +0300

    init frontend

--- backend/src/config/app.config.ts
+++
@@ -1,9 +0,0 @@
-// config/app.config.ts
-export default () => ({
-  app: {
-    name: process.env.APP_NAME || 'saloonBackend',
-    env: process.env.NODE_ENV || 'development',
-    port: parseInt(process.env.PORT || '3000', 10),
-  },
-
-});
--- backend/src/modules/saloon/dto/update-saloon.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSaloonDto } from './create-saloon.dto';
-
-export class UpdateSaloonDto extends PartialType(CreateSaloonDto) {}
